b584e89b3a2556365125580a74e47e04d0293dfb
Dan Abdinoor <abdinoor@gmail.com>
2018-05-31 12:38:17 -0400

    reading itunes txt files and cleaning up fields

--- xspfy.py
+++ xspfy.py
@@ -16,16 +16,16 @@ SPOTIFY_CLIENT_SECRET = '6186195c2bf34bd6a2caf05d76f157fc'
 SPOTIFY_AUTH_SCOPE    = 'playlist-modify-public playlist-modify-private'
 SPOTIFY_REDIRECT_URI  = 'https://sepehr.github.io/laspotipy'
 
-def spotify_uri(artist, track, album = False):
+def spotify_uri(token, artist, track, album = False):
     '''
     Returns spotify uri by artist and track names using Spotify Web API.
     '''
-    spotify = spotipy.Spotify()
+    spotify = spotipy.Spotify(token)
 
     if album:
-        query = 'artist:%s album:%s %s' % (artist, album, track)
+        query = 'artist:"%s" album:"%s" %s' % (artist, album, track)
     else:
-        query = 'artist:%s %s' % (artist, track)
+        query = 'artist:"%s" %s' % (artist, track)
 
     try:
         response = spotify.search(q = query, type = 'track', offset = 0, limit = 1)
@@ -34,7 +34,8 @@ def spotify_uri(artist, track, album = False):
             return response['tracks']['items'][0]['uri']
 
         return False
-    except:
+    except Exception as ex:
+        print ex
         return False
 
 
@@ -51,7 +52,8 @@ def spotify_playlist_create(token, username, name, public = True):
             return False
 
         return response['id']
-    except:
+    except Exception as ex:
+        print ex
         return False
 
 
@@ -106,6 +108,94 @@ def xspf_parse(path):
     return pl.playlist
 
 
+class Playlist(object):
+    '''
+    its a playlist
+    '''
+
+class Track(object):
+    '''
+    its a track in a playlist
+    '''
+
+
+def txt_parse(path):
+    '''
+    Parse a text file into an array of ['artist', 'track'].
+    '''
+
+
+    playlist = Playlist()
+    playlist.title = '.'.join(path.split('/')[-1].split('.')[:-1])  # filename cleaned
+    playlist.track = []
+
+
+    # import codecs
+    # f = codecs.open(path, encoding='utf-8')
+    # f.seek(0)
+    # print repr(f.readline()[:1])
+    # f.close()
+
+
+    import codecs
+
+    with codecs.open(path, encoding='utf-16') as f:
+        lines = f.read().splitlines()
+        for line in lines[1:]:
+            try:
+                fields = line.split('\t')
+                track = Track()
+                track.title   = clean_track_name(fields[0])
+                track.creator = clean_artist(fields[1])
+                track.album   = fields[3]
+                playlist.track.append(track)
+            except IndexError as ex:
+                print line, ex
+                continue
+            except Exception as ex:
+                print line, ex
+                raise ex
+
+    return playlist
+
+
+def clean_bad_chars(input):
+    clean = input.lower()
+    clean = clean.replace('(', '')
+    clean = clean.replace(')', '')
+    clean = clean.replace('[', '')
+    clean = clean.replace(']', '')
+    # clean = clean.replace(',', ' ')
+    # clean = clean.replace('.', ' ')
+    clean = clean.replace('&', ', ')
+    # clean = clean.replace("'", '')
+    clean = ' '.join(clean.split())  # consolidate spaces last
+    return clean
+
+
+def clean_features(input):
+    clean = input
+    clean = clean.replace('ft.', 'feat.')
+    if 'feat.' in clean:
+        clean = clean.split('feat.')[0]
+    return clean
+
+
+def clean_track_name(track_name):
+    clean = clean_bad_chars(track_name)
+    clean = clean_features(clean)
+    return clean
+
+
+
+def clean_artist(artist):
+    clean = clean_bad_chars(artist)
+    clean = clean_features(clean)
+    if ',' in clean:
+        clean = clean.split(',')[0]
+    return clean
+
+
 def main():
     # -------------------------------------------------------------------------
     # Check args, init
@@ -113,95 +203,68 @@ def main():
     if len(sys.argv) < 3:
         sys.exit('\nUSAGE: xspfy.py XSPF_PATH SPOTIFY_USERNAME\n')
     else:
-        xspf_path    = sys.argv[1]
-        spotify_user = sys.argv[2].lower()
-
-    # Introduce
-    print '''
-
-    XSPFy is a utility to migrate your [lastfm-exported] XSPF playlists to your
-    Spotify account where you can ACTUALLY play them, collaborate on them with
-    your friends, etc.
-
-    You need to authorize XSPFy to access to your Spotify account in order to be
-    able to create playlists. After processing XSPF playlists you will be presented with a
-    new page opened in your browser. You need to login to Spotify service (if not logged in)
-    and grant the required permissions to XSPFy. If granted, you will be redirected to a new
-    URL (sepehr.github.com/laspotipy), copy the whole URL and paste it to XSPFy CLI application
-    where it waits and waits for you, the lord, to be blessed by the permissions.
-
-    OK, no more bullshit. Have fun, and listen to good music :)
-
-    P.S. Find my music profiles at:
-    http://last.fm/user/lajevardi
-    http://play.spotify.com/user/sepehrlajevardi
-
-    '''
-
-    # Remove this, seriously!
-    raw_input('Press any key to continue...')
-
-    spotify_pls = []
-    xspf_paths  = xspf_playlist_paths(xspf_path)
-
-    if not xspf_paths:
-        sys.exit('[ERROR] Could not find any XSPF playlists, is there any?')
-
-    print 'Found %d XSPF playlist(s)' % len(xspf_paths)
+        xspf_path    = sys.argv[2]
+        spotify_user = sys.argv[1].lower()
 
     print '\nConnecting to Spotify API endpoint, authorizing as "%s"...' % spotify_user
 
     token = spotify_auth_token(spotify_user, SPOTIFY_AUTH_SCOPE, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI)
 
-    for xspf_path in xspf_paths:
-        print '\nProcessing "%s"...' % xspf_path
+    os.chdir(xspf_path)
+    for file in glob.glob("*.txt"):
+        process(file, token, spotify_user)
 
-        pl = xspf_parse(xspf_path)
 
-        if not pl.track:
-            print '[ERROR] Could not fetch tracks for this playlist, is there any? Skipping...'
-            continue
+def process(xspf_path, token, spotify_user):
+    print '\nProcessing "%s"...' % xspf_path
 
-        failed = count = 0
-        pl_len = len(pl.track)
-        spotify_pl = {
-            'title':  pl.title,
-            'tracks': []
-        }
+    pl = txt_parse(xspf_path)
 
-        print 'Found %d tracks in the playlist:' % pl_len
+    failed = count = 0
+    pl_len = len(pl.track)
+    spotify_pl = {
+        'title':  pl.title,
+        'tracks': []
+    }
 
-        for track in pl.track:
-            uri = spotify_uri(track.creator, track.title)
+    print 'Found %d tracks in the playlist:' % pl_len
 
-            count += 1
-            print '\t%d/%d: "%s - %s"' % (count, pl_len, track.creator[:40], track.title[:40])
+    spotify_pls = []
 
-            if uri:
-                print '\t[FOUND] %s\n' % uri
-                spotify_pl['tracks'].extend([uri])
+    for track in pl.track:
+        uri = spotify_uri(token, track.creator, track.title)
 
-            else:
-                print '\t[FAILED]\n'
-                failed += 1
+        count += 1
+        print '\t%d/%d: "%s - %s"' % (count, pl_len, track.creator[:40], track.title[:40])
 
-        spotify_pls.extend([spotify_pl])
+        if uri:
+            print '\t[FOUND] %s\n' % uri
+            spotify_pl['tracks'].extend([uri])
 
-        print '%d tracks not found on Spotify.' % failed
+        else:
+            print '\t[FAILED]\n'
+            failed += 1
+            sys.exit(failed)
 
-        print 'Creating Spotify playlist with %d found tracks: "%s"' % (len(spotify_pl['tracks']), spotify_pl['title'])
+    spotify_pls.extend([spotify_pl])
 
-        pl_id = spotify_playlist_create(token, spotify_user, spotify_pl['title'])
-        if not pl_id:
-            print '[ERROR] Could not create Spotify playlist. Skipping...'
-            continue
+    print '%d tracks not found on Spotify.' % failed
 
-        success = spotify_playlist_add(token, spotify_user, pl_id, spotify_pl['tracks'])
-        if not success:
-            print '[ERROR] Could not add tracks to the playlist. Please make sure to REMOVE the playlist manually.'
-            continue
+    print 'Creating Spotify playlist with %d found tracks: "%s"' % (len(spotify_pl['tracks']), spotify_pl['title'])
 
-        print '[SUCCESS] Enjoy!'
+    pl_id = spotify_playlist_create(token, spotify_user, spotify_pl['title'])
+    if not pl_id:
+        print '[ERROR] Could not create Spotify playlist. Skipping...'
+        return
+
+    success = spotify_playlist_add(token, spotify_user, pl_id, spotify_pl['tracks'])
+    if not success:
+        print '[ERROR] Could not add tracks to the playlist. Please make sure to REMOVE the playlist manually.'
+        return
+
+    print '[SUCCESS] Enjoy!'
+
+    os.unlink(xspf_path)
 
 
 if __name__ == '__main__':
